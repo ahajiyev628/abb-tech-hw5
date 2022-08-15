f9caa3d1669faaa35291da4830f8d6792ab48795
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-08-15 21:30:43 +0400

    Update Family.java

--- Family.java
+++ Family.java
@@ -1,86 +1,73 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Objects;
 
-public class Family extends Human {
-    public Family(String name, String surname, int year, Human mother, Human father) {
-        super(name, surname, year, mother, father);
+public class Family {
+    Pet pet = new Pet();
+    Human mother = new Human("Allahverdi","Hajiyev");
+    Human father = new Human("Allahverdi", "Hajiyev");
+    Human[] children = new Human[0];
+
+    public Family(Pet pet, Human mother, Human father, Human[] children) {
+        this.pet = pet;
+        this.mother = mother;
+        this.father = father;
+        this.children = children;
     }
 
-    static Human mother = new Family("Tito", "Karleone",1960,new Human("Allahverdi", "Hajiyev"), new Human("Allahverdi", "Hajiyev"));
-    static Human father = new Family("Vito", "Karleone", 1945, new Human("Allahverdi", "Hajiyev"), new Human("Allahverdi", "Hajiyev"));
+    public Family() {
+    }
 
-    static Human child = new Family("Micheal", "Karleone", 1977, new Human(mother.getName(), mother.getSurname()), new Human(father.getName(), father.getSurname()));
-
-    static Pet p = new Pet("dog", "Rock", 5, new String[]{"eat", "drink", "sleep"});
-
-    static ArrayList<Human> parents = new ArrayList<>();
-    static ArrayList<Human> children = new ArrayList<>();
-    static ArrayList<Pet> pets = new ArrayList<>();
-
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args){
-        parents.add(mother);
-        parents.add(father);
-        children.add(child);
-        pets.add(p);
-
-        HashMap<Integer, ArrayList<Human>> family = new HashMap<>();
-        family.put(1, parents);
-        family.put(2, children);
-
-        System.out.println("number of children in the family: " + children.size());
-        System.out.println("Add new child to the family");
-        addChild(new Human("","",0, new Human("",""), new Human("","")));
-        System.out.println("number of children in the family: " + children.size());
-
-        // System.out.println("number of family member: " + (parents.size() + children.size()));
-
-        countFamily(family);
-
-        System.out.println(child.toString());
-        System.out.println(father.toString());
-        System.out.println(mother.toString());
-
-        /*
+    public Human[] deleteChild(Human child) {
         int i = 0;
-        for(String weekday : Human.weekdays) {
-            Human.schedule[i++][0] = weekday;
+        if (children.length > 1) {
+            Human[] newChildren = new Human[children.length - 1];
+            for (Human ch : children) {
+                if (!ch.equals(child)) {
+                    newChildren[i] = ch;
+                    i++;
+                }
+            }
+            this.children = newChildren;
         }
-        int j = 0;
-        for (String task : Human.tasks) {
-            Human.schedule[j++][1] = task;
+        return children;
+    }
+
+    public Human[] addChild(Human child) {
+        Human[] newChildren = new Human[children.length + 1];
+        newChildren[children.length] = child;
+        for (int i = 0; i < children.length; i++) {
+            newChildren[i] = children[i];
         }
-        */
-    }
-    public static Human addChild(Human ch) {
-        System.out.print("Name: ");
-        ch.setName(sc.nextLine());
-        System.out.print("Surname: ");
-        ch.setSurname(sc.nextLine());
-        System.out.print("Year: ");
-        ch.setYear(sc.nextInt());
-
-        ch = new Family(ch.getName(),
-                        ch.getSurname(),
-                        ch.getYear(),
-                        new Human(father.getName(), father.getSurname()),
-                        new Human(mother.getName(),mother.getSurname()));
-
-        children.add(ch);
-        System.out.println("Child was added");
-        System.out.println(children.get(1).toString());
-        return ch;
+        this.children = newChildren;
+        return children;
     }
 
-    public static void deleteChild(int position) {
-        children.remove(position);
-        System.out.println(position + "th child was removed from family");
+    public void countFamily() {
+        System.out.printf("number of family member: %d", children.length + 2);
     }
 
-    public static void countFamily(Map<Integer, ArrayList<Human>> fm) {
-        System.out.println("number of family member: " + (fm.get(1).size() + fm.get(2).size()));
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(pet, family.pet) && Objects.equals(mother, family.mother) && Objects.equals(father, family.father) && Arrays.equals(children, family.children);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(pet, mother, father);
+        result = 31 * result + Arrays.hashCode(children);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "pet=" + pet +
+                ", mother=" + mother +
+                ", father=" + father +
+                ", children=" + Arrays.toString(children) +
+                '}';
     }
 }
