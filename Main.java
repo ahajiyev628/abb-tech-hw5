0de740315f102a403e5abd99dc9bf7f13629a658
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-08-21 23:46:39 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,74 @@
+package homework5;
+
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         Human mother = new Human();
-        Human father = new Human();
-        Human child = new Human();
-        Pet p = new Pet();
-        Family family = new Family();
+        mother.setName("mother name");
+        mother.setSurname("mother surname");
+        //mother.setFamily(family1);
 
+        Human father = new Human();
+        father.setName("father name");
+        father.setSurname("father surname");
+        //father.setFamily(family1);
+
+        Pet p = new Pet();
+        p.setSpecies("dog");
+        p.setNickname("wimel");
+        p.setTrickLevel(80);
+        p.setAge(5);
+
+        Family family1 = new Family(p, father, mother);
+        System.out.println(family1.toString());
+
+        Human child = new Human();
+        child.setMother(mother);
+        child.setFather(father);
+        child.setPet(p);
+        child.setFamily(family1);
         child.setName("Michael");
         child.setSurname("Karleone");
         child.setYear(1990);
         child.setIq(50);
 
-        Human[] childs = family.addChild(child);
-        for(Human ch : childs) System.out.println(ch.toString());
+        family1.addChild(child);
+        child.toString();
+        System.out.println("");
+        family1.toString();
+        System.out.println("");
+        family1.countFamily();
+        System.out.println("");
+        System.out.println("After first child added");
 
-        // family.deleteChild(0);
+        System.out.println(family1.toString());
 
-        family.countFamily();
+        Human child2 = new Human();
+        child2.setMother(mother);
+        child2.setFather(father);
+        child2.setPet(p);
+        child2.setFamily(family1);
+        child2.setName("Michael2");
+        child2.setSurname("Karleone2");
+        child2.setYear(1992);
+        child2.setIq(52);
+
+        System.out.println("After first second added");
+        System.out.println(family1.toString());
+        family1.addChild(child2);
+        family1.countFamily();
+        System.out.println("");
+
+//
+//        Human[] childs = family1.addChild(child);
+//        for(Human ch : childs) System.out.println(ch.toString());
+
+        System.out.println("After second child deleted");
+        family1.deleteChild(1);
+
+        family1.countFamily();
 
         // System.out.println(family.toString());
 
